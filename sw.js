62b1ee59a7038aba698c018eb72968a08b38fda0
Rodrigo <rodrigoazancotmenezes@gmail.com>
2026-09-23 16:39:16 +0100

    Add Nextbots home screen

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 importScripts('./offline-catalog.js');
-const SHELL = 'rod-shell-7de24848872a0348';
+const SHELL = 'rod-shell-a5d643cae1a8bc75';
 const PREFIX = 'rod-game-';
 const BASE = new URL('./', self.location.href);
 const absolute = p => new URL(p, BASE).href;
